aece68bae87bffb70e527381dbd402c5e87d27a9
Khoa Nguyen <khoacts@gmail.com>
2026-09-14 23:35:01 +0700

    Cap nhat cu phap TemplateResponse theo chuan Starlette moi

--- app/templating.py
+++ app/templating.py
@@ -1,6 +1,7 @@
+from fastapi import Request
 from fastapi.templating import Jinja2Templates
 
-templates = Jinja2Templates(directory="app/templates")
+_templates = Jinja2Templates(directory="app/templates")
 
 
 def vnd(value) -> str:
@@ -11,4 +12,20 @@ def vnd(value) -> str:
         return str(value)
 
 
-templates.env.filters["vnd"] = vnd
+_templates.env.filters["vnd"] = vnd
+
+
+class Templates:
+    """Boc lai de dung duoc ca hai cach goi TemplateResponse."""
+
+    def __init__(self, inner: Jinja2Templates):
+        self._inner = inner
+        self.env = inner.env
+
+    def TemplateResponse(self, name, context=None, **kwargs):
+        # Cu phap moi cua Starlette: Request dung truoc, ten template sau
+        request: Request = context.pop("request")
+        return self._inner.TemplateResponse(request, name, context, **kwargs)
+
+
+templates = Templates(_templates)
